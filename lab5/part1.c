913e5706facf1d5a38ec19998776e0c40c6b247f
Kenneth Chan <kchan049@ucr.edu>
2016-10-15 22:07:48 -0700

    Create part1.c

---
+++ lab5/part1.c
@@ -0,0 +1,30 @@
+
+unsigned char GetBit(unsigned char x, unsigned char k)
+{
+	return ((x & (0x01 << k)) != 0);
+}
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b)
+{
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+
+unsigned char shift_data = 0;
+
+// Ensure DDRC is setup as output
+void transmit_data(unsigned char data) {
+	unsigned char i;
+	for (i = 0; i < 8 ; ++i) {
+		// Sets SRCLR to 1 allowing data to be set
+		// Also clears SRCLK in preparation of sending data
+		PORTC = 0x08;
+		// set SER = next bit of data to be sent.
+		PORTC |= ((data >> i) & 0x01);
+		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+		PORTC |= 0x04;
+	}
+	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
+	PORTC |= 0x02;
+	// clears all lines in preparation of a new transmission
+	PORTC = 0x00;
+}
