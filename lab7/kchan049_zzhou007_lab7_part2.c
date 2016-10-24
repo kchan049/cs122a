15c6302e1331a51a0aaa5cf0f25c1cceb9dd552a
Kenneth Chan <kchan049@ucr.edu>
2016-10-24 11:06:16 -0700

    Update kchan049_zzhou007_lab7_part2.c

--- lab7/kchan049_zzhou007_lab7_part2.c
+++ lab7/kchan049_zzhou007_lab7_part2.c
@@ -1,7 +1,42 @@
-#include <avr/io.h>
+/*	Partner(s) Name & E-mail: Kenneth Chan (kchan049@ucr.edu) and Zihang Zhou(zzhou007@ucr.edu)
+ *	Lab Section: 21 
+ *	Assignment: Lab #7  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <stdint.h>
 #include <stdlib.h>
-#include "C:\Users\student\Desktop\scheduler.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/portpins.h>
+#include <avr/pgmspace.h>
 
+//FreeRTOS include files
+#include "FreeRTOS.h"
+#include "task.h"
+#include "croutine.h"
+//variables
+unsigned row = 0xFE;
+int r = 0;
+unsigned char ARRAY[5] = {
+	0x01, 0x00, 0x00, 0x00, 0x00
+};
+int mid;
+int mid2;
+
+void cal() {
+	Set_A2D_Pin(0x00);
+	mid = ADC;
+	Set_A2D_Pin(0x01);
+	mid2 = ADC;
+}
 void A2D_init() {
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 	// ADEN: Enables analog-to-digital conversion
@@ -9,128 +44,152 @@ void A2D_init() {
 	// ADATE: Enables auto-triggering, allowing for constant
 	//	    analog to digital conversions.
 }
+void Set_A2D_Pin(unsigned char pinNum) {
+	ADMUX = (pinNum <= 0x07) ? pinNum : ADMUX;
+	// Allow channel to stabilize
+	static unsigned char i = 0;
+	for ( i=0; i<15; i++ ) { asm("nop"); }
+}
+unsigned char p_x, p_y;
+void left() {
+	if (p_x < 7) {
+		ARRAY[p_y] <<= 1;
+		++p_x;
+	}
+}
+void right() {
+	if (p_x > 0) {
+		ARRAY[p_y] >>= 1;
+		--p_x;
+	}
+}
+void swap(unsigned char a, unsigned char b) {
+	unsigned char temp = ARRAY[a];
+	ARRAY[a] = ARRAY[b];
+	ARRAY[b] = temp;
+}
+void up() {
+	if (p_y < 4)
+	{
+		swap(p_y,p_y+1);
+		++p_y;
+	}
+}
+void down() {
+	if (p_y > 0)
+	{
+		swap(p_y,p_y-1);
+		--p_y;
+	}
+}
+enum SM1_States { sm1_s1 } sm1_state;
+enum SM2_States { sm2_s1 } sm2_state;
 
-
-enum SM2_States { SM2_s1 } SM2_State;
-unsigned row = 0xFE;
-int r = 0;
-unsigned char ARRAY[5] = {
-	0x01, 0x00, 0x00, 0x00, 0x00
-};
-int TickFct_State_machine_2(int state) {
-	switch(state) { // Transitions
-		case -1:
-		state = SM2_s1;
-		break;
-		case SM2_s1:
-		if (1) {
-			state = SM2_s1;
-		}
-		break;
-		default:
-		state = -1;
-	} // Transitions
-
-	switch(state) { // State actions
-		case SM2_s1:
-		if (r == 4) {
-			row = 0xFE;
-			r = 0;
-		}
-		else {
-			row = (row << 1) | 0x01;
-			++r;
-		}
-
-		PORTC = ARRAY[r];
-		PORTD = row;
-		break;
-		default: // ADD default behaviour below
-		break;
-	} // State actions
-	SM2_State = state;
-	return state;
+void SM1_Init() {
+	sm1_state = sm1_s1;
+}
+void SM2_Init() {
+	sm2_state = sm2_s1;
 }
 
-enum SM1_States { SM1_s1 } SM1_State;
 
-int TickFct_State_machine_1(int state) {
-	/*VARIABLES MUST BE DECLARED STATIC*/
-	/*e.g., static int x = 0;*/
-	/*Define user variables for this state machine here. No functions; make them global.*/
-	static unsigned char p_x, p_y;
-	static int timer = 0;
-	switch(state) { // Transitions
-		case -1:
-		p_x = 0;
-
-		p_y = 0;
-		SM1_State = SM1_s1;
-		break;
-		case SM1_s1:
-		if (1) {
-			SM1_State = SM1_s1;
-			timer+=100;
-		}
-		break;
+void SM1_Tick() {
+	switch(sm1_state) { // Transitions
+		case sm1_s1:
+			sm1_state = sm1_s1;
+			break;
 		default:
-		SM1_State = SM1_s1;
+			break;
 	} // Transitions
 
-	switch(SM1_State) { // State actions
-		case SM1_s1:
-		// period is 100 ms
-		if (ADC <= 48) {
-			// may need to move this to function later
-			if (p_x < 7) {
-				ARRAY[0] <<= 1;
-				++p_x;
-			}
-		}
-		else if (ADC >= 512) {
-			if (p_x > 0) {
-				ARRAY[0] >>= 1;
-				--p_x;
-			}
-		}
-		break;
-		default: // ADD default behaviour below
-		break;
+	switch(sm1_state) { // State actions
+		case sm1_s1:
+			Set_A2D_Pin(0x00);
+				if (ADC < mid - 20) {
+					left();
+				}
+				else if (ADC > mid + 40) {
+					right();
+				}
+		
+			Set_A2D_Pin(0x01);
+				if (ADC < mid2 - 20) {
+					up();
+				}
+				else if (ADC > mid2 + 40) {
+					down();
+				}
+			break;
+		default:
+			break;
 	} // State actions
-	return SM1_State;
 }
 
-int main(void)
-{
+void SM2_Tick() {
+	switch(sm2_state) { // Transitions
+		case sm2_s1:
+			sm2_state = sm2_s1;
+			break;
+		default:
+			break;
+	} // Transitions
+
+	switch(sm2_state) { // State actions
+		case sm2_s1:
+			if (r == 4) {
+				row = 0xFE;
+				r = 0;
+			}
+			else {
+				row = (row << 1) | 0x01;
+				++r;
+			}
+
+			PORTC = ARRAY[r];
+			PORTD = row;
+			break;
+		default: 
+			break;
+	} // State actions
+}
+
+void SM1Task() {
+	SM1_Init();
+	for(;;)
+	{
+		SM1_Tick();
+		vTaskDelay(100);
+	}
+}
+void SM2Task() {
+	SM2_Init();
+	for(;;)
+	{
+		SM2_Tick();
+		vTaskDelay(2);
+	}
+}
+void StartSecPulse1(unsigned portBASE_TYPE Priority) {
+	xTaskCreate(SM1Task, (signed portCHAR *)"SM1Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+}
+void StartSecPulse2(unsigned portBASE_TYPE Priority) {
+	xTaskCreate(SM2Task, (signed portCHAR *)"SM2Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+}
+
+int main(void) {
+	//init
+	A2D_init();
 	// initialize ports
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
+	//Start Tasks
+	cal();
+	StartSecPulse1(1);
+	StartSecPulse2(1);
+	//RunSchedular
+	vTaskStartScheduler();
 	
-	tasksNum = 2; // declare number of tasks
-	task tsks[2]; // initialize the task array
-	tasks = tsks; // set the task array
-	
-	A2D_init();
-	
-	// define tasks
-	unsigned char i=0; // task counter
-	tasks[i].state = -1;
-	tasks[i].period = 2;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFct = &TickFct_State_machine_2;
-	++i;
-	tasks[i].state = -1;
-	tasks[i].period = 500;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFct = &TickFct_State_machine_1;
-	++i;
-	
-	TimerSet(2); // value set should be GCD of all tasks
-	TimerOn();
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	return 0;
 }
