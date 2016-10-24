a98dff386011ad3d13363002beacc6b03bd105d6
Zi Zhou <zzhou007@ucr.edu>
2016-10-24 09:39:24 -0700

    Update part1.c

--- lab7/part1.c
+++ lab7/part1.c
@@ -1,6 +1,27 @@
-#include <avr/io.h>
+/*	Partner(s) Name & E-mail: Kenneth Chan (kchan049@ucr.edu) and Zihang Zhou(zzhou007@ucr.edu)
+ *	Lab Section: 21 
+ *	Assignment: Lab #2  Exercise #4 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <stdint.h>
 #include <stdlib.h>
-#include "C:\Users\student\Desktop\scheduler.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/portpins.h>
+#include <avr/pgmspace.h>
+
+//FreeRTOS include files
+#include "FreeRTOS.h"
+#include "task.h"
+#include "croutine.h"
 
 void A2D_init() {
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
@@ -10,59 +31,55 @@ void A2D_init() {
 	//	    analog to digital conversions.
 }
 
-enum SM2_States { SM2_s1 } SM2_State;
-int TickFct_State_machine_2(int state) {
-	switch(state) { // Transitions
-		case -1:
-		state = SM2_s1;
-		break;
-		case SM2_s1:
-		if (1) {
-			state = SM2_s1;
-		}
-		break;
-		default:
-		state = -1;
-	} // Transitions
-
-	switch(state) { // State actions
-		case SM2_s1:
-			PORTB = (unsigned char)(ADC & 0xFF);
-			PORTD = (unsigned char)((ADC >> 8) << 5);
-		break;
-		default: // ADD default behaviour below
-		break;
-	} // State actions
-	SM2_State = state;
-	return state;
+enum SM1_States { sm1_s1 } sm1_state;
+	
+void SM1_Init() {
+	sm1_state = sm1_s1;
 }
 
-int main(void)
-{
+void SM1_Tick() {
+	switch(sm1_state) { // Transitions
+		case sm1_s1:
+			sm1_state = sm1_s1;
+			break;
+		default:
+			break;
+	} // Transitions
+
+	switch(sm1_state) { // State actions
+		case sm1_s1:
+			PORTB = (unsigned char)(ADC & 0xFF);
+			PORTD = (unsigned char)((ADC >> 8) << 5);
+			break;
+		default:
+			break;
+	}
+}
+
+void SM1Task() {
+	SM1_Init();
+	for(;;)
+	{
+		SM1_Tick();
+		vTaskDelay(1);
+	}
+}
+
+void StartSecPulse1(unsigned portBASE_TYPE Priority) {
+	xTaskCreate(SM1Task, (signed portCHAR *)"SM1Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+}
+
+int main(void) {
 	// initialize ports
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
+	//Start Tasks
+	StartSecPulse1(1);
+	//RunSchedular
+	vTaskStartScheduler();
 	
-	tasksNum = 1; // declare number of tasks
-	task tsks[1]; // initialize the task array
-	tasks = tsks; // set the task array
-	
-	A2D_init();
-	
-	// define tasks
-	unsigned char i=0; // task counter
-	tasks[i].state = -1;
-	tasks[i].period = 2;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFct = &TickFct_State_machine_2;
-	++i;
-	
-	TimerSet(2); // value set should be GCD of all tasks
-	TimerOn();
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	return 0;
 }
+
