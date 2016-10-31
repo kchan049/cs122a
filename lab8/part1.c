e244df79586e8b0add5e1edfd06a6046a5472139
Zi Zhou <zzhou007@ucr.edu>
2016-10-31 09:14:35 -0700

    Update part1.c

--- lab8/part1.c
+++ lab8/part1.c
@@ -1,25 +1,46 @@
+/*	Partner(s) Name & E-mail: Kenneth Chan (kchan049@ucr.edu) and Zihang Zhou(zzhou007@ucr.edu)
+ *	Lab Section: 21 
+ *	Assignment: Lab #2  Exercise #4 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
 #include <avr/io.h>
-#include "C:\Users\student\Desktop\scheduler.h"
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/portpins.h>
+#include <avr/pgmspace.h>
+
+//FreeRTOS include files
+#include "FreeRTOS.h"
+#include "task.h"
+#include "croutine.h"
 
 unsigned char phase[8] = {1,3,2,6,4,12,8,9};
 unsigned char counter = 0;
-enum BL_States { SM1_On };
-int TickFct_BlinkLED(int state) {
-	switch(state) { // Transitions
-		case -1: // Initial transition
-		state = SM1_On;
-		break;
-		case SM1_On:
-			state = SM1_On;
-		break;
+enum SM1_States { on } sm1_state;
+
+void SM1_Tick() {
+	switch(sm1_state) { // Transitions
+
+		case on:
+			sm1_state = on;
+			break;
 		default:
-		state = -1;
+			break;
 	}
 
-	switch(state) { // State actions
-		case SM1_On:
+	switch(sm1_state) { // State actions
+		case on:
 			if (~PINB & 0x03 == 3) {
-				
+			
 			}
 			else if (~PINB & 0x01) {
 				if (counter < 7)
@@ -34,37 +55,42 @@ int TickFct_BlinkLED(int state) {
 				counter = 7;
 			}
 		
-		PORTA = phase[counter];
-		break;
+			PORTA = phase[counter];
+			break;
 		default:
-		break;
+			break;
 	}
-	
-	return state;
+}
+
+
+void SM1_Init() {
+	sm1_state = on;
+}
+
+
+void SM1Task() {
+	SM1_Init();
+	for(;;)
+	{
+		SM1_Tick();
+		vTaskDelay(100);
+	}
+}
+
+void StartSecPulse1(unsigned portBASE_TYPE Priority) {
+	xTaskCreate(SM1Task, (signed portCHAR *)"SM1Task", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
 }
 
 int main(void) {
-	
+	//init
 	// initialize ports
 	DDRA = 0xFF; PORTA = 0x00;
 	DDRB = 0x00; PORTB = 0xFF;
 	DDRD = 0xFF; PORTD = 0x00;
+	//Start Tasks
+	StartSecPulse1(1);
+	//RunSchedular
+	vTaskStartScheduler();
 	
-	tasksNum = 1; // declare number of tasks
-	task tsks[1]; // initialize the task array
-	tasks = tsks; // set the task array
-	unsigned char pp = 3;
-	// define tasks
-	unsigned char i=0; // task counter
-	tasks[i].state = -1;
-	tasks[i].period = pp;
-	tasks[i].elapsedTime = tasks[i].period;
-	tasks[i].TickFct = &TickFct_BlinkLED;
-	++i;
-	
-	TimerSet(pp); // value set should be GCD of all tasks
-	TimerOn();
-
-	while(1) {} // task scheduler will be called by the hardware interrupt
-	
+	return 0;
 }
